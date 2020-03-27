5cde6d47a844a4040478cd98e059f0c7c420b464
jdeleonardis <jdeleonardis@hotmail.com>
2020-03-27 11:35:54 -0400

    Multiple changes to stylying, including code to fit smaller screens

--- script/script.js
+++ script/script.js
@@ -1,58 +1,80 @@
 var mainForm = document.getElementById("mainForm");
-var startScreen = document.getElementById("startScreen");
-var viewHighScoreEl = document.getElementById("viewHighScore")
+var screenContainerEl = document.getElementById("screenContainer");
+var viewHighScoreEl = document.getElementById("highScoreLink");
+var highScoreScreenEl = document.getElementById("highScoreScreen");
 var startBtnEl = document.getElementById("startBtn");
+var clearHighScoresBtnEl = document.getElementById("clearHighScoresBtn");
 var timerEl = document.getElementById("timerP");
 var timerDisplayEl = document.getElementById("timerDisplay");
-var ulHolderEl = document.createElement("ul");
+var ulHolderEl = document.createElement("ol");
 var currentScoreEl = document.getElementById("currentScore");
+var resultEl = document.getElementById("results");
 var currentResultEl = document.getElementById("questionFeedBack");
 
-var countDownTimer = 30;
+var countDownTimer = 60;
 var questionIndex = 0;
 var quizScore = 0;
 
-//start button listener
-startBtnEl.addEventListener("click", function(event) {
-  event.preventDefault();
-  startTheTimer();
-});
+viewHighScoreEl.addEventListener("click",viewHighScores);
+startBtnEl.addEventListener("click", startTheTimer);    
+// clearHighScoresBtnEl.addEventListener("click", startTheTimer); 
 
 //Countdown timer
-function startTheTimer() {
+function startTheTimer(event) {
+  event.preventDefault();
     showQuestion(questionIndex);
     var timerInterval = setInterval(function() {
       countDownTimer--;
       timerDisplayEl.textContent =countDownTimer + " seconds left";    
       if(countDownTimer <= 0) {
-        endGame()
+        finishGame()
         clearInterval(timerInterval);
       }   
-
     }, 1000);
+}
+
+function viewHighScores(event){
+  if (highScoreScreenEl.classList.contains("hide")){
+    screenContainerEl.setAttribute("class","hide");
+    resultEl.setAttribute("class","results hide");
+    highScoreScreenEl.setAttribute("class","show");
+    viewHighScoreEl.textContent = "Back to quiz";
+  }
+  else {
+    resultEl.setAttribute("class","results show");
+    screenContainerEl.setAttribute("class","show");
+    highScoreScreenEl.setAttribute("class","hide");
+    viewHighScoreEl.textContent = "View High Scores";
+  }
+}
+
+function saveHighScores(event){
+  event.preventDefault();
+  alert("here");
 }
 
 //code to show the questions
 function showQuestion(questionIndex){
-  startScreen.textContent = "";
+  screenContainerEl.textContent = "";
   ulHolderEl.textContent = "";
 
   //get the question and array of answers at the question index
   var userQuestion = question[questionIndex].questionTitle;
   var possibleAnswers = question[questionIndex].answerChoices;
   //add the question to the start screen div
-  startScreen.textContent = userQuestion;
+  screenContainerEl.textContent = userQuestion;
 
   //Add the possible answers to the screen for each question
   possibleAnswers.forEach(function (newListItem) {
     var showAnswers = document.createElement("li");
     showAnswers.innerHTML = ' <button id="answerBtn" class="btn btn-primary">' + newListItem + '</button>';
-    startScreen.appendChild(ulHolderEl);
+    screenContainerEl.appendChild(ulHolderEl);
     ulHolderEl.appendChild(showAnswers);
     showAnswers.addEventListener("click", (answerChecker));
   })
 }
 
+//code that checks answers and processes
 function answerChecker(event){
   var choice = event.target;
 
@@ -78,7 +100,7 @@ function answerChecker(event){
 
     if (questionIndex == question.length){
       //call ending function here        
-      endGame();
+      finishGame();
     }
     else
     {
@@ -88,8 +110,9 @@ function answerChecker(event){
   }
 }
 
-function endGame(){
-  startScreen.textContent = "";
+//finish the game
+function finishGame(){
+  screenContainerEl.textContent = "";
   currentResultEl.textContent = "";
   timerEl.style.display = "none";
 
@@ -97,32 +120,33 @@ function endGame(){
   var endGameH1El = document.createElement("h1");
   endGameH1El.setAttribute("id", "endGameH1El");
   endGameH1El.textContent = "Game over man!"
-  startScreen.appendChild(endGameH1El);
+  screenContainerEl.appendChild(endGameH1El);
 
   //p tag that shows your final score 
   var endGamePEl = document.createElement("p");
   endGamePEl.setAttribute("id", "endGameP");
   endGamePEl.textContent = "Your final score is: " + quizScore;
-  startScreen.appendChild(endGamePEl);
+  screenContainerEl.appendChild(endGamePEl);
   
   //label for the name input
   var newLabelEl = document.createElement("label");
   newLabelEl.setAttribute("id", "inputLabelEl");
   newLabelEl.setAttribute("for", "inputEl");
   newLabelEl.textContent = "Enter your name or initials: ";
-  startScreen.appendChild(newLabelEl);
+  screenContainerEl.appendChild(newLabelEl);
 
   //Input element to enter name
-  var newInputEl = document.createElement("input");
-  newInputEl.setAttribute("type", "text");
-  newInputEl.setAttribute("id", "inputEl");
-  newInputEl.textContent = "";
-  startScreen.appendChild(newInputEl);
+  var nameInputEl = document.createElement("input");
+  nameInputEl.setAttribute("type", "text");
+  nameInputEl.setAttribute("id", "inputEl");
+  nameInputEl.textContent = "";
+  screenContainerEl.appendChild(nameInputEl);
 
   //Submit button, which will record high scores.
-  var newSubmitEl = document.createElement("button");
-  newSubmitEl.setAttribute("id", "startBtn");
-  newSubmitEl.setAttribute("class", "btn btn-primary");
-  newSubmitEl.textContent = "Submit";
-  startScreen.appendChild(newSubmitEl);
+  var highScoreSaveBtnEl = document.createElement("button");
+  highScoreSaveBtnEl.setAttribute("id", "startBtn");
+  highScoreSaveBtnEl.setAttribute("class", "btn btn-primary");
+  highScoreSaveBtnEl.textContent = "Save";
+  highScoreSaveBtnEl.addEventListener("click",saveHighScores);
+  screenContainerEl.appendChild(highScoreSaveBtnEl);
 }
